63b5485c506498d89768807e3701121f1b2ed8e4
Lautaro Sosa <lautarososa9999@gmail.com>
2024-07-27 21:16:21 -0300

    landing 2.0

--- my-project/src/pages/Customizer.jsx
+++ my-project/src/pages/Customizer.jsx
@@ -151,14 +151,6 @@ const Customizer = () => {
             </div>
           </motion.div>
 
-          <motion.div className="absolute top-20 right-12" {...fadeAnimation}>
-            <CustomButton
-              type="filled"
-              title="Go Back"
-              handleClick={() => (state.intro = true)}
-              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
-            />
-          </motion.div>
 
           <motion.div
             // className="filtertabs-container"
--- my-project/src/pages/Landing.jsx
+++ my-project/src/pages/Landing.jsx
@@ -1,74 +1,89 @@
 import Navbar from "../components/Navbar";
+import { PiTShirtBold } from "react-icons/pi";
+import { FaPen } from "react-icons/fa6";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { RiImageAddFill } from "react-icons/ri";
+
 export default function Component() {
   return (
-    <div className="flex flex-col min-h-screen mx-[5%] bg-slate-900 text-gray-300">
-      <Navbar />
-      <main className="flex-1">
-        <section id="about" className="w-full py-12 md:py-24 lg:py-32 ">
-          <div className="container px-4 md:px-6">
-            <div className="grid gap-6 lg:grid-cols-2 lg:gap-12">
-              <div className="">
-                <div className="flex flex-col ">
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-gray-200 mb-16">
-                    Personalización de logotipos y camisetas
-                  </h1>
-                  <p className="text-muted-foreground md:text-xl text-gray-400 mb-20">
-                    Deja volar tu creatividad con un sinfin de posibilidades de
-                    como se vería tu camiseta con tu logo o diseño
-                    personalizado.
-                  </p>
-                  <div className="mx-auto my-6">
-                    <a
-                      href="/customizer"
-                      className=" bg-orange-500 p-6 px-10 md:px-16 rounded-xl text-2xl hover:bg-orange-400  "
-                    >
-                      Comenzar
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <img
-                src="/bg.jpg"
-                alt="About"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full"
-              />
-            </div>
+    <div className="min-h-screen bg-gradient-to-b from-black to-blue-900 text-white">
+      <header className="flex items-center justify-between p-6">
+        <div className="text-4xl font-bold flex gap-3">
+          <img src="favicon.png" alt="Logo icon" className="h-10" />
+          <span className="text-blue-400">T-Shirt </span>
+          <span className="text-pink-400">Custom</span>
+        </div>
+      </header>
+      <main className="px-4 py-10 text-center md:px-6">
+        <section id="about" className="w-full py-10 md:py-24 lg:py-32 ">
+          <h1 className="text-4xl font-bold md:text-5xl">
+            Tu camiseta<span className="text-pink-500"> Imaginada</span>
+          </h1>
+          <p className="mt-4 text-lg md:text-xl">
+            Crea tu propia camiseta sin limite de creatividad de manera gratuita
+          </p>
+          <div className="mt-6 space-x-4">
+            <a
+              href="/customizer"
+              className="bg-pink-600 hover:bg-fuchsia-600  text-xl text-white font-semibold p-5 rounded-xl "
+            >
+              Empieza a crear gratis
+            </a>
           </div>
         </section>
 
-        <section >
-          <div className="flex w-[900px] mx-auto items-center justify-center relative">
+        <section>
+          <div className="mt-12 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
             <img
               src="/canvas (2).png"
-              alt="canvas"
-              className="h-[20rem] absolute top-0 left-20 overflow-hidden "
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
             />
             <img
               src="/canvas (2).png"
-              alt="canvas"
-              className="h-[20rem] absolute top-0 left-0 overflow-hidden "
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
             />
             <img
               src="/canvas (2).png"
-              alt="canvas"
-              className="h-[20rem]  overflow-hidden "
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
+            />
+            <img
+              src="/canvas (2).png"
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
+            />
+            <img
+              src="/canvas (2).png"
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
+            />
+            <img
+              src="/canvas (2).png"
+              alt="T-shirt design 1"
+              className="rounded-lg bg-gray-700/30 hover:scale-105"
             />
           </div>
         </section>
 
-        <section
-          id="services"
-          className="w-full py-12 md:py-24 lg:py-32 bg-muted"
-        >
-          <div className="container px-4 md:px-6">
+        <div className="mt-6 flex justify-around bg-black/40 p-4">
+          <p>✨ Cada camiseta es única!</p>
+          <p>✨ Deja volar tu imaginacion!</p>
+          <p>✨ Infinitas posibilidades!</p>
+          {/* <p>✨ Unimaginable scenes!</p> */}
+        </div>
+
+        <section id="about" className="w-full py-10 md:py-24 lg:py-32 ">
+          <div className=" px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold  sm:text-4xl tracking-wide text-gray-200">
                   Servicio
                 </h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Podras customizar de varias maneras como quieres que se vea tu
-                  remera, luego guardar la imagen para poder usarlas a tu gusto
+                  Customiza sin limites como quieres que se vea tu
+                  remera, luego guardar la imagen para poder usarlas a tu gusto.
                 </p>
               </div>
             </div>
@@ -98,8 +113,7 @@ export default function Component() {
                         </span>
                       </h3>
                       <p className="text-muted-foreground">
-                        Selecione a cor da camiseta para combinar com seu
-                        estilo.
+                        Selecciona el color de tu camiseta para combinar con tu estilo.
                       </p>
                     </div>
                   </li>
@@ -120,27 +134,68 @@ export default function Component() {
                 </ul>
               </div>
               <img
-                src="/bg.jpg"
+                src="/customizer2.png"
                 alt="Services"
-                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
+                className="mx-auto overflow-hiddeno hover:scale-105 rounded-xl object-cover object-center sm:w-full"
               />
             </div>
           </div>
         </section>
-        <section id="projects" className="w-full py-12 md:py-24 lg:py-32">
-          <div className="container px-4 md:px-6">
-            <div className="flex flex-col items-center justify-center space-y-4 text-center">
-              <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
-                  Nossos Projetos
-                </h2>
-                <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Veja alguns dos projetos que concluímos para nossos clientes
-                  satisfeitos.
+
+        <section className="py-12 md:py-20 lg:py-28">
+          <div className="container mx-auto px-4 md:px-6">
+            <div className="grid gap-8 md:grid-cols-3">
+              <div className="flex flex-col items-center text-center">
+                <div className="flex gap-1">
+                  <PiTShirtBold size={50} />
+                  <FaPen size={20}/>
+                </div>
+                <h3 className="mt-4 text-xl font-semibold">
+                  Customizacion de{" "}
+                  <span className="font-extrabold text-blue-400 text-2xl">
+                    logos / dibujos
+                  </span>
+                </h3>
+                <p className="mt-2 text-muted-foreground">
+                  Our product is designed for lightning-fast performance,
+                  ensuring your business stays ahead of the curve.
+                </p>
+              </div>
+              <div className="flex flex-col items-center text-center">
+              <IoColorPaletteOutline size={50}/>
+                {/* <ShieldIcon className="h-12 w-12 text-gray" /> */}
+                <h3 className="mt-4 text-xl font-semibold">
+                  Cambia de{" "}
+                  <span className="font-extrabold text-blue-400 text-2xl">
+                    colores
+                  </span>
+                </h3>
+                <p className="mt-2 text-muted-foreground">
+                  Trust our robust security features and reliable infrastructure
+                  to protect your data and keep your business running smoothly.
+                </p>
+              </div>
+              <div className="flex flex-col items-center text-center">
+                {/* <SettingsIcon className="h-12 w-12 text-gray" /> */}
+                <RiImageAddFill size={50}/>
+                <h3 className="mt-4 text-xl font-semibold">
+                  Sube tus{" "}
+                  <span className="font-extrabold text-blue-400 text-2xl">
+                    imagenes
+                  </span>
+                </h3>
+                <p className="mt-2 text-muted-foreground">
+                  Trust our robust security features and reliable infrastructure
+                  to protect your data and keep your business running smoothly.
                 </p>
               </div>
             </div>
-            <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-2 lg:gap-12">
+          </div>
+        </section>
+
+        <section id="projects" className="w-full py- md:py-24 lg:py-8">
+          <div className=" px-4 md:px-6">
+            <div className="mx-auto grid max-w-5xl items-center gap-6 py-8 lg:grid-cols-2 lg:gap-12">
               <img
                 src="/bg.jpg"
                 alt="Project 1"
@@ -149,9 +204,9 @@ export default function Component() {
               <div className="flex flex-col justify-center space-y-4">
                 <h3 className="text-2xl font-bold">Simple y gratuito</h3>
                 <p className="text-muted-foreground md:text-xl/relaxed">
-                  Desenvolvemos uma plataforma web personalizada para uma
-                  empresa do setor de varejo, otimizando seus processos e
-                  melhorando a experiência do usuário.
+                  Desarrollamos una plataforma web personalizada para un empresa
+                  del sector retail, optimizando sus procesos y mejorando la
+                  experiencia del usuario.
                 </p>
               </div>
             </div>
@@ -189,7 +244,7 @@ export default function Component() {
             className="text-xs hover:underline underline-offset-4 text-muted-foreground"
             prefetch={false}
           >
-            Redes Sociais
+            Redes sociales
           </a>
         </nav>
       </footer>
@@ -197,21 +252,3 @@ export default function Component() {
   );
 }
 
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  );
-}
